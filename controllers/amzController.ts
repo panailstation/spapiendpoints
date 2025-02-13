76cdaaadd1f016f634f3170b0f6e6570de8e4cd5
Opeyemi <yemijoshua81@gmail.com>
2025-02-13 07:19:35 +0100

    updating dates

--- controllers/amzController.ts
+++ controllers/amzController.ts
@@ -82,7 +82,7 @@ const getOrders = async (req: Request, res: Response) => {
 
   try {
     const createdAfter = "2023-09-01T00:00:00Z";
-    const createdBefore = "2023-12-31T23:59:59Z";
+    const createdBefore = "2023-11-31T23:59:59Z";
     let authTokens = await authenticate();
     const baseUrl = `${endpoint}/orders/v0/orders`;
 
